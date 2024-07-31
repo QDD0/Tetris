fff1710a00fbf6caceb79270c0cfa6a743ab9df9
Den <dend51108@gmail.com>
2024-07-31 17:10:50 +0300

    Изменил временно main полностью

--- Tetris.cpp
+++ Tetris.cpp
@@ -4,23 +4,34 @@
 const unsigned int X = 400;
 const unsigned int Y = 600;
 
+enum class GameState {
+    Menu,
+    Game
+};
+
 class Tetris {
 public:
+    void Run();
 };
 
 class Menu {
 public:
-    void Program();
+    void Display(sf::RenderWindow& window);
+    bool HandleClick(sf::Vector2f mousePos);
 };
 
-void Menu::Program() {
-    sf::RenderWindow window(sf::VideoMode(X, Y), "Tetris");
+class Game {
+public:
+    void Display(sf::RenderWindow& window);
+};
 
+void Menu::Display(sf::RenderWindow& window) {
     sf::RectangleShape button(sf::Vector2f(80, 20));
     button.setFillColor(sf::Color::Red);
+    button.setPosition(X / 2 - button.getSize().x / 2, Y / 2 - button.getSize().y / 2);
 
     sf::Text text;
-    sf::Text Windowtext;
+    sf::Text StartText;
     sf::Font font;
 
     if (!font.loadFromFile("C:\\Users\\denis\\Desktop\\Project\\Tetris\\Times New Roman.ttf")) {
@@ -33,17 +44,44 @@ void Menu::Program() {
     text.setCharacterSize(15);
     text.setFillColor(sf::Color::White);
 
-    Windowtext.setFont(font);
-	Windowtext.setString("TETRIS");
-	Windowtext.setCharacterSize(30);
-	Windowtext.setFillColor(sf::Color::White);
-    Windowtext.setPosition(150,200);
-
-    button.setPosition(X / 2 - button.getSize().x / 2, Y / 2 - button.getSize().y / 2);
+	StartText.setFont(font);
+	StartText.setString("TETRIS");
+	StartText.setCharacterSize(30);
+	StartText.setFillColor(sf::Color::White);
+    StartText.setPosition(150, 200);
 
     sf::FloatRect textRect = text.getLocalBounds();
     text.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
     text.setPosition(button.getPosition().x + button.getSize().x / 2, button.getPosition().y + button.getSize().y / 2);
+
+    window.clear();
+    window.draw(button);
+	window.draw(StartText);
+    window.draw(text);
+    window.display();
+}
+
+bool Menu::HandleClick(sf::Vector2f mousePos) {
+    sf::RectangleShape button(sf::Vector2f(80, 20));
+    button.setPosition(X / 2 - button.getSize().x / 2, Y / 2 - button.getSize().y / 2);
+
+    if (button.getGlobalBounds().contains(mousePos)) {
+        return true;
+    }
+    return false;
+}
+
+void Game::Display(sf::RenderWindow& window) {
+    window.clear();
+    // Отображение игрового экрана (здесь можно добавить игровой контент)
+    window.display();
+}
+
+void Tetris::Run() {
+    sf::RenderWindow window(sf::VideoMode(X, Y), "Tetris");
+    GameState state = GameState::Menu;
+    Menu menu;
+    Game game;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -51,19 +89,28 @@ void Menu::Program() {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+            if (state == GameState::Menu && event.type == sf::Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    sf::Vector2f mousePos(event.mouseButton.x, event.mouseButton.y);
+                    if (menu.HandleClick(mousePos)) {
+                        state = GameState::Game;
+                    }
+                }
+            }
         }
 
-        window.clear();
-        window.draw(button);
-        window.draw(Windowtext);
-        window.draw(text);
-        window.display();
+        if (state == GameState::Menu) {
+            menu.Display(window);
+        }
+        else if (state == GameState::Game) {
+            game.Display(window);
+        }
     }
 }
 
 int main() {
-    Menu menu;
-    menu.Program();
+    Tetris tetris;
+    tetris.Run();
 
     return 0;
 }
